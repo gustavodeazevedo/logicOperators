9e6d64f4d961788ead1964954b8b8e0d45d6a318
Gustavo de Azevedo <gustavodeazevedo2003@outlook.com>
2024-08-02 23:31:47 -0300

    vers.:1.0

---
+++ _script.js/operator_&&.js
@@ -0,0 +1,19 @@
+function logicalAnd_1(val1, val2) {
+    console.log(val1 && val2);
+}
+
+let var1 = true;
+let var2 = true;
+
+logicalAnd_1(var1, var2);
+
+
+function logicalAnd_2(var1, var2) {
+    console.log(var1 && var2);
+}
+
+var1 = true;
+var2 = false;
+
+logicalAnd_2(var1, var2); //* boolean logic expression
+
